20564c23632fc82ec7e108dff201be435fde6623
akahoshi1421 <akahoshi1421@users.noreply.github.com>
2023-06-15 22:12:39 +0000

    deploy: 0cf0bff6a51f24a63e5ba81f659bf6076ff344b5

--- _next/static/CA6kHNcl13E917EDABOAz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d4c3101123efcb30.css","static/chunks/pages/index-276e2c62a61200ac.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
